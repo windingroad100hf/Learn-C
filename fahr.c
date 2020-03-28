0fb75b7a4c2e0144a5da6157bddaf8f6724534ca
Harrison Fritz <windingroad100hf@gmail.com>
2020-03-28 11:31:44 -0400

    Initial commit with a few c files

---
+++ fahr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* convert farenheit into Celcius */
+
+int convert(int fahr)
+{
+	int celsius;
+	celsius = (5.0/9.0)*(fahr-32);
+	return celsius;
+}
+
+int main()
+{
+      int a=212;
+      int d=convert(a);
+      printf("%d Farenheit is %d in Celsius\n", a, d);
+}
+
+
---
+++ main.c
@@ -0,0 +1,5 @@
+# include <stdio.h>
+int main()
+{
+    printf("\a\v\v\vn  \bHello world!\n\xb");
+    printf("/xb");}
